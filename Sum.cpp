106ce14500fc87d77afe6d77b53d01951f9a5d6e
Harry19s <96477632+Harry19s@users.noreply.github.com>
2022-04-12 10:15:18 +0530

    Add files via upload

---
+++ Sum.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+int main (){
+    int a,b,c;
+    cout<<"Enter two numbers:";
+    cin>>a>>b;
+    c=a+b;
+    cout<<"Addition of two number is = "<<c<<endl;
+    return 0;
+}
